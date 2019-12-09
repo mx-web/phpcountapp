d7da2c63f77e43044092ffcf715b7d9ae9eb828b
Max Walter <mwa@ad.neos-it.de>
2019-12-09 11:10:03 +0100

    New code from Marius

--- index.php
+++ index.php
@@ -1,22 +1,55 @@
 <?php
+// Load the Dotenv Module to use it for parsing the .env file
 require __DIR__ . '/vendor/autoload.php';
 use Symfony\Component\Dotenv\Dotenv;
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__.'/.env');
 
-$pdo = new PDO('mysql:host='.$_ENV['MYSQL_HOST'].';dbname='. $_ENV['MYSQL_DB'].'', $_ENV['MYSQL_USER'], $_ENV['MYSQL_PASSWORD']);
+$mysqli = new mysqli($_ENV['MYSQL_HOST'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_USER'], $_ENV['MYSQL_DB']);
+if ($mysqli->connect_errno) {
+    die("Verbindung fehlgeschlagen: " . $mysqli->connect_error);
+}
 
-echo "<pre>";
-
-$stmt = $pdo->prepare("SELECT * FROM counter ORDER BY id DESC LIMIT 1");
+$stmt = $mysqli->prepare("SHOW TABLES LIKE 'under_construction'"); // Put your table name after Like '
 $stmt->execute();
-$result = $stmt->fetchAll();
-$count = $result[0]['count'];
+$result = $stmt->get_Result();
+$resultConverted = mysqli_fetch_all($result, MYSQLI_ASSOC);
+if(empty($resultConverted)) {
+    $stmt = $mysqli->prepare("CREATE TABLE under_construction (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        ip_adress VARCHAR(255) NOT NULL,
+        a TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+      )
+      ");
+    echo $stmt->execute();
+}
 
-$stmt = $pdo->prepare("INSERT INTO counter (count, timestp) VALUES (?, ?)");
-$params = [++$count, date("Y-m-d",time())];
-$stmt->execute($params);
+function getUserIpAddress() {
+    $arrToCheck = ['HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR','HTTP_FORWARDED', 'REMOTE_ADDR'];
+    foreach ($arrToCheck as $http_constant) {
+        if (isset($_SERVER[$http_constant])) {
+            return $_SERVER[$http_constant];
+        }
+    }
+    return 'UNKNOWN!';
+}
 
-echo "UNDER_CONSTRUCTION_UNTIL = " . $_ENV['UNDER_CONSTRUCTION_UNTIL'] . '<br>';
-echo "YOUR REMOTE ADDRESS = " . $_SERVER['REMOTE_ADDR'] . ' <br>';
-echo "COUNT = ". $count;
+$ipAdress = getUserIpAddress();
+
+// Track information about the entering client (ip_adress)
+$stmt = $mysqli->prepare("INSERT INTO under_construction (ip_adress) VALUES (?)");
+$stmt->bind_param("s", $ipAdress);
+$stmt->execute();
+// ip_adress tracked
+
+
+// Start fetching information about User and get the highest id value to print it out
+$stmt = $mysqli->prepare('SELECT id FROM under_construction ORDER BY id DESC LIMIT 1');
+$stmt->execute();
+$result = $stmt->get_Result();
+$resultConverted = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// Output in HTML
+echo '<h1>Under construction until ' . $_ENV['UNDER_CONSTRUCTION_UNTIL'] . '</h1><br>';
+echo 'Your ip-adress is: ' . $ipAdress . '<br>';
+echo 'This site was visited by ' . $resultConverted[0]['id'] . ' users';
